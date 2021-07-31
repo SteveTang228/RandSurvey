e8bf3655d577eadacc7d2f1b2496a34ec95f3566
SteveTang228 <74914010+SteveTang228@users.noreply.github.com>
2021-07-31 16:14:44 +0800

    Add url

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
-<html>
-<head>
-  <!-- 這裡是 HTML 語法的 header 頁首引用宣告區 -->
-  <!-- 這裡是 HTML 語法的 header 頁首引用宣告區 -->
-  <!-- 這裡是 HTML 語法的 header 頁首引用宣告區 -->
-</head>
-<body>
-  <!-- 這裡是 HTML 語法的 主要資料區 -->
+<?php
+$arr = array(
+"0" => "http://www.google.com",
+"1" => "http://www.Facebook.com",
+"2" => "http://www.youtube.com",
+"3" => "http://www.yahoo.com.tw",
+);
 
-  <?php echo "放上首頁介紹的資料";?>
-
-  <!-- 這裏通常會放 footer 頁尾的資料 -->
-  <!-- 這裏通常會放 footer 頁尾的資料 -->
-  <!-- 這裏通常會放 footer 頁尾的資料 -->
-</body>
-</html>
+$a = mt_rand(0,3);
+$url = $arr[$a];
+header( "Location: $url" );
+?>
